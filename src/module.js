a38fa46e4edaac6275a493e4da7b5aa10d641522
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2018-06-05 19:27:13 +0900

    annotation support

--- src/module.js
+++ src/module.js
@@ -1,5 +1,6 @@
 import { AwsCloudWatchLogsDatasource } from './datasource';
 import { AwsCloudWatchLogsDatasourceQueryCtrl } from './query_ctrl';
+import { AwsCloudWatchLogsAnnotationsQueryCtrl } from './annotations_query_ctrl';
 
 class AwsCloudWatchLogsDatasourceConfigCtrl { }
 AwsCloudWatchLogsDatasourceConfigCtrl.templateUrl = 'partials/config.html';
@@ -7,5 +8,6 @@ AwsCloudWatchLogsDatasourceConfigCtrl.templateUrl = 'partials/config.html';
 export {
   AwsCloudWatchLogsDatasource as Datasource,
   AwsCloudWatchLogsDatasourceQueryCtrl as QueryCtrl,
-  AwsCloudWatchLogsDatasourceConfigCtrl as ConfigCtrl
+  AwsCloudWatchLogsDatasourceConfigCtrl as ConfigCtrl,
+  AwsCloudWatchLogsAnnotationsQueryCtrl as AnnotationsQueryCtrl
 };
