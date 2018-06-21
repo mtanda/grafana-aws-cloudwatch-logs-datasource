c032fe327c44f8d4106388381ea39d3ab4252ca2
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2018-06-21 15:33:19 +0900

    sugget log group/stream name

--- src/query_ctrl.js
+++ src/query_ctrl.js
@@ -1,7 +1,7 @@
 import { QueryCtrl } from 'app/plugins/sdk';
 
 export class AwsCloudWatchLogsDatasourceQueryCtrl extends QueryCtrl {
-  constructor($scope, $injector) {
+  constructor($scope, $injector, templateSrv) {
     super($scope, $injector);
 
     this.scope = $scope;
@@ -10,6 +10,30 @@ export class AwsCloudWatchLogsDatasourceQueryCtrl extends QueryCtrl {
     this.target.logGroupName = this.target.logGroupName || '';
     this.target.logStreamNames = this.target.logStreamNames || [];
     this.target.filterPattern = this.target.filterPattern || '';
+    this.templateSrv = templateSrv;
+
+    this.suggestLogGroupName = (query, callback) => {
+      let region = this.target.region || this.defaultRegion;
+      return this.datasource.doMetricQueryRequest('log_group_names', {
+        region: this.templateSrv.replace(region),
+        prefix: query
+      }).then(data => {
+        callback(data.map(d => { return d.value; }))
+      });
+    };
+
+    this.suggestLogStreamName = (query, callback) => {
+      if (!this.target.logGroupName) {
+        return callback([]);
+      }
+      let region = this.target.region || this.defaultRegion;
+      return this.datasource.doMetricQueryRequest('log_stream_names', {
+        region: this.templateSrv.replace(region),
+        logGroupName: this.target.logGroupName
+      }).then(data => {
+        callback(data.map(d => { return d.value; }))
+      });
+    };
   }
 
   onChangeInternal() {
